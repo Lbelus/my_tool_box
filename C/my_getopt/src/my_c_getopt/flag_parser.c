a8b3a144412258b820b23bdda0e089544080d899
LBelus <lorris.belus@icloud.com>
2023-12-07 23:36:10 +0000

    add getopt

---
+++ C/my_getopt/src/my_c_getopt/flag_parser.c
@@ -0,0 +1,28 @@
+#include <my_cgetopt.h>
+
+int flag_parser(int argc, char** argv, const char* valid_args, my_getopt_t* getopt_ptr)
+{
+    if (argc < 2)
+    {
+        return EXIT_FAILURE;
+    }
+    int opt             = 0;
+    getopt_ptr->index   = 0;
+    getopt_ptr->str_pos = 0;
+    getopt_ptr->nbr_str = 0;
+    count_str_array(argc, argv, getopt_ptr);
+    if (getopt_ptr->nbr_str > 0)
+    {
+        getopt_ptr->str_arr = (char**)malloc(sizeof(char*) * (getopt_ptr->nbr_str));
+        alloc_array(argc, argv, getopt_ptr);
+    }
+    while (getopt_ptr->index < argc)
+    {
+        if ((opt = my_getopt(argv, valid_args, getopt_ptr)) != - 1)
+        {
+            flag_state(opt, getopt_ptr, argv);
+        }
+        opt = 0;
+    }
+    return EXIT_SUCCESS;
+}
---
+++ C/my_getopt/src/my_c_getopt/init_getopt.c
@@ -0,0 +1,10 @@
+#include <my_cgetopt.h>
+
+void init_getopt(my_getopt_t* getopt_ptr, const char* valid_args)
+{
+    getopt_ptr->exit_status = false;
+    int len = _my_strlen(valid_args);
+    getopt_ptr->bool_arr = (bool*)malloc(sizeof(bool) * len);
+    fill_bool_array(getopt_ptr->bool_arr, _my_strlen(valid_args));
+    getopt_ptr->nbr_str = 0;
+}
